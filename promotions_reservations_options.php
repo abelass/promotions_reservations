53ff7a57ba70cb4f0984ced2c6d0abfcfcb6a83d
abelass <rainer@websimple.be>
2018-04-19 11:34:18 +0200

    les promotions vont vers le plugin dédié
    https://github.com/abelass/promotions_reservations

--- promotions_reservations_options.php
+++ promotions_reservations_options.php
@@ -13,12 +13,5 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
-
-/*
- * Un fichier d'options permet de définir des éléments
- * systématiquement chargés à chaque hit sur SPIP.
- *
- * Il vaut donc mieux limiter au maximum son usage
- * tout comme son volume !
- * 
- */
+// Ajoute le plugin aux promotions.
+$GLOBALS['promotion_plugin']['reservation_evenement'] = _T('reservation_evenement:reservation_evenement_titre');
