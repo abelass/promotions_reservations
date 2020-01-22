2b1a2524d35e31b78b6843b6dd6b7eeaa14375a9
Rainer Müller <rainer@websimple.be>
2020-01-22 17:41:59 +0100

    activement maintenu

--- promotions_reservations_options.php
+++ promotions_reservations_options.php
@@ -3,7 +3,7 @@
  * Options au chargement du plugin Promotions Réservations
  *
  * @plugin     Promotions Réservations
- * @copyright  2018
+ * @copyright  2018 - 2020
  * @author     Rainer Müller
  * @licence    GNU/GPL
  * @package    SPIP\Promotions_reservations\Options
